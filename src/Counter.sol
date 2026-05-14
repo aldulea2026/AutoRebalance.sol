43e285bc46d9a2e36b9a8b514c762057cdb183c0
aldulea2026 <mazenaldulea2026@gmail.com>
2026-05-14 03:29:49 +0300

    Update Solidity version and refactor imports

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,67 +1,137 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.26;
 
-import {BaseHook} from "https://raw.githubusercontent.com/Uniswap/v4-periphery/main/src/base/hooks/BaseHook.sol";
-import {IPoolManager} from "https://raw.githubusercontent.com/Uniswap/v4-core/main/src/interfaces/IPoolManager.sol";
-import {Hooks} from "https://raw.githubusercontent.com/Uniswap/v4-core/main/src/libraries/Hooks.sol";
-import {PoolKey} from "https://raw.githubusercontent.com/Uniswap/v4-core/main/src/types/PoolKey.sol";
-import {BalanceDelta} from "https://raw.githubusercontent.com/Uniswap/v4-core/main/src/types/BalanceDelta.sol";
-import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "https://raw.githubusercontent.com/Uniswap/v4-core/main/src/types/BeforeSwapDelta.sol";
+import {Test} from "forge-std/Test.sol";
 
-contract AlduleaSmartHook is BaseHook {
-    uint256 public constant COMMISSION_BPS = 1000; // 10%
+import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
+import {CurrencyLibrary, Currency} from "@uniswap/v4-core/src/types/Currency.sol";
+import {StateLibrary} from "@uniswap/v4-core/src/libraries/StateLibrary.sol";
+import {LiquidityAmounts} from "@uniswap/v4-core/test/utils/LiquidityAmounts.sol";
+import {IPositionManager} from "@uniswap/v4-periphery/src/interfaces/IPositionManager.sol";
+import {Constants} from "@uniswap/v4-core/test/utils/Constants.sol";
 
-    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+import {EasyPosm} from "./utils/libraries/EasyPosm.sol";
 
-    function getHookPermissions()
-        public
-        pure
-        override
-        returns (Hooks.Permissions memory)
-    {
-        return
-            Hooks.Permissions({
-                beforeInitialize: false,
-                afterInitialize: true,
-                beforeAddLiquidity: false,
-                afterAddLiquidity: false,
-                beforeRemoveLiquidity: false,
-                afterRemoveLiquidity: false,
-                beforeSwap: true,
-                afterSwap: true,
-                beforeDonate: false,
-                afterDonate: false,
-                beforeSwapReturnDelta: false,
-                afterSwapReturnDelta: false,
-                afterAddLiquidityReturnDelta: false,
-                afterRemoveLiquidityReturnDelta: false
-            });
+import {Counter} from "../src/Counter.sol";
+import {BaseTest} from "./utils/BaseTest.sol";
+
+contract CounterTest is BaseTest {
+    using EasyPosm for IPositionManager;
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+    using StateLibrary for IPoolManager;
+
+    Currency currency0;
+    Currency currency1;
+
+    PoolKey poolKey;
+
+    Counter hook;
+    PoolId poolId;
+
+    uint256 tokenId;
+    int24 tickLower;
+    int24 tickUpper;
+
+    function setUp() public {
+        // Deploys all required artifacts.
+        deployArtifactsAndLabel();
+
+        (currency0, currency1) = deployCurrencyPair();
+
+        // Deploy the hook to an address with the correct flags
+        address flags = address(
+            uint160(
+                Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
+                    | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+            ) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
+        );
+        bytes memory constructorArgs = abi.encode(poolManager); // Add all the necessary constructor arguments from the hook
+        deployCodeTo("Counter.sol:Counter", constructorArgs, flags);
+        hook = Counter(flags);
+
+        // Create the pool
+        poolKey = PoolKey(currency0, currency1, 3000, 60, IHooks(hook));
+        poolId = poolKey.toId();
+        poolManager.initialize(poolKey, Constants.SQRT_PRICE_1_1);
+
+        // Provide full-range liquidity to the pool
+        tickLower = TickMath.minUsableTick(poolKey.tickSpacing);
+        tickUpper = TickMath.maxUsableTick(poolKey.tickSpacing);
+
+        uint128 liquidityAmount = 100e18;
+
+        (uint256 amount0Expected, uint256 amount1Expected) = LiquidityAmounts.getAmountsForLiquidity(
+            Constants.SQRT_PRICE_1_1,
+            TickMath.getSqrtPriceAtTick(tickLower),
+            TickMath.getSqrtPriceAtTick(tickUpper),
+            liquidityAmount
+        );
+
+        (tokenId,) = positionManager.mint(
+            poolKey,
+            tickLower,
+            tickUpper,
+            liquidityAmount,
+            amount0Expected + 1,
+            amount1Expected + 1,
+            address(this),
+            block.timestamp,
+            Constants.ZERO_BYTES
+        );
     }
 
-    function beforeSwap(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.SwapParams calldata params,
-        bytes calldata hookData
-    )
-        external
-        override
-        returns (bytes4, BeforeSwapDelta, uint24)
-    {
-        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+    function testCounterHooks() public {
+        // positions were created in setup()
+        assertEq(hook.beforeAddLiquidityCount(poolId), 1);
+        assertEq(hook.beforeRemoveLiquidityCount(poolId), 0);
+
+        assertEq(hook.beforeSwapCount(poolId), 0);
+        assertEq(hook.afterSwapCount(poolId), 0);
+
+        // Perform a test swap //
+        uint256 amountIn = 1e18;
+        BalanceDelta swapDelta = swapRouter.swapExactTokensForTokens({
+            amountIn: amountIn,
+            amountOutMin: 0, // Very bad, but we want to allow for unlimited price impact
+            zeroForOne: true,
+            poolKey: poolKey,
+            hookData: Constants.ZERO_BYTES,
+            receiver: address(this),
+            deadline: block.timestamp + 1
+        });
+        // ------------------- //
+
+        assertEq(int256(swapDelta.amount0()), -int256(amountIn));
+
+        assertEq(hook.beforeSwapCount(poolId), 1);
+        assertEq(hook.afterSwapCount(poolId), 1);
     }
 
-    function afterSwap(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.SwapParams calldata params,
-        BalanceDelta delta,
-        bytes calldata hookData
-    )
-        external
-        override
-        returns (bytes4, int128)
-    {
-        return (BaseHook.afterSwap.selector, 0);
+    function testLiquidityHooks() public {
+        // positions were created in setup()
+        assertEq(hook.beforeAddLiquidityCount(poolId), 1);
+        assertEq(hook.beforeRemoveLiquidityCount(poolId), 0);
+
+        // remove liquidity
+        uint256 liquidityToRemove = 1e18;
+        positionManager.decreaseLiquidity(
+            tokenId,
+            liquidityToRemove,
+            0, // Max slippage, token0
+            0, // Max slippage, token1
+            address(this),
+            block.timestamp,
+            Constants.ZERO_BYTES
+        );
+
+        assertEq(hook.beforeAddLiquidityCount(poolId), 1);
+        assertEq(hook.beforeRemoveLiquidityCount(poolId), 1);
     }
 }
